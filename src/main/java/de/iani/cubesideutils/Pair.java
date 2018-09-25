19efbe99db1bd993d20dde1be3c5ce6d142ebdc3
Jonas Becker <jonas.becker@uni-muenster.de>
2018-09-25 14:04:06 +0200

    Add class Pair.

---
+++ src/main/java/de/iani/cubesideutils/Pair.java
@@ -0,0 +1,14 @@
+package de.iani.cubesideutils;
+
+
+public class Pair<T> {
+
+    public final T first;
+    public final T second;
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+}
