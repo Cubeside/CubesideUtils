e594bcedf2c43c9e3d3699fdee96f867d6e4f4f7
Brokkonaut <hannos17@gmx.de>
2018-10-30 04:41:15 +0100

    Add custom heads

---
+++ src/main/java/de/iani/cubesideutils/items/CustomHeads.java
@@ -0,0 +1,47 @@
+package de.iani.cubesideutils.items;
+
+import java.util.UUID;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import com.destroystokyo.paper.profile.PlayerProfile;
+import com.destroystokyo.paper.profile.ProfileProperty;
+
+public enum CustomHeads {
+    QUARTZ_ARROW_UP("3554e03b-982d-44f1-8be4-71785ba822f8", "White Arrow Up", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMWFkNmM4MWY4OTlhNzg1ZWNmMjZiZTFkYzQ4ZWFlMmJjZmU3NzdhODYyMzkwZjU3ODVlOTViZDgzYmQxNGQifX19"), //
+    QUARTZ_ARROW_DOWN("8f54d1c4-c599-4c54-8993-2cb371649c33", "White Arrow Down", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvODgyZmFmOWE1ODRjNGQ2NzZkNzMwYjIzZjg5NDJiYjk5N2ZhM2RhZDQ2ZDRmNjVlMjg4YzM5ZWI0NzFjZTcifX19"),//
+
+    ;
+
+    private ItemStack head;
+
+    private CustomHeads(String ownerUUIDString, String ownerName, String texturesProperty) {
+        head = createHead(UUID.fromString(ownerUUIDString), ownerName, texturesProperty);
+    }
+
+    public ItemStack getHead() {
+        return new ItemStack(head);
+    }
+
+    public ItemStack getHead(String displayName) {
+        ItemStack stack = getHead();
+        ItemMeta meta = stack.getItemMeta();
+        meta.setDisplayName(displayName);
+        stack.setItemMeta(meta);
+        return stack;
+    }
+
+    public static ItemStack createHead(UUID ownerUUID, String ownerName, String texturesProperty) {
+        ItemStack stack = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta meta = (SkullMeta) stack.getItemMeta();
+        PlayerProfile profile = Bukkit.createProfile(ownerUUID, ownerName);
+        profile.setProperty(new ProfileProperty("textures", texturesProperty));
+        meta.setPlayerProfile(profile);
+        stack.setItemMeta(meta);
+        return stack;
+    }
+}
