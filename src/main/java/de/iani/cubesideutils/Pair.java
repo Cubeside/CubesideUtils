0a8aad09c7539e7315da01cd510293c8f49e7bac
Jonas Becker <jonas.becker@uni-muenster.de>
2019-04-17 19:27:29 +0200

    Make Pair ConfigurationSerializable.

--- src/main/java/de/iani/cubesideutils/Pair.java
+++ src/main/java/de/iani/cubesideutils/Pair.java
@@ -1,7 +1,16 @@
 package de.iani.cubesideutils;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
 
-public class Pair<T, S> {
+public class Pair<T, S> implements ConfigurationSerializable {
+
+    static {
+        ConfigurationSerialization.registerClass(Pair.class);
+    }
 
     public final T first;
     public final S second;
@@ -9,6 +18,39 @@ public class Pair<T, S> {
     public Pair(T first, S second) {
         this.first = first;
         this.second = second;
+    }
+
+    @SuppressWarnings("unchecked")
+    public Pair(Map<String, Object> serialized) {
+        this.first = (T) serialized.get("first");
+        this.second = (S) serialized.get("second");
+    }
+
+    @Override
+    public Map<String, Object> serialize() {
+        Map<String, Object> result = new LinkedHashMap<>(2);
+        result.put("first", this.first);
+        result.put("second", this.second);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + this.first + ";" + this.second + ")";
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.first) + 31 * Objects.hashCode(this.second);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof Pair<?, ?>)) {
+            return false;
+        }
+        Pair<?, ?> op = (Pair<?, ?>) other;
+        return Objects.equals(this.first, op.first) && Objects.equals(this.second, op.second);
     }
 
 }
