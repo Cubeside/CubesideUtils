db1d48f684535a3d025f218354d6106ba9b77d13
Jonas Becker <jonas.becker@uni-muenster.de>
2019-07-25 19:16:24 +0200

    Add HybridCommand.

---
+++ src/main/java/de/iani/cubesideutils/commands/HybridCommand.java
@@ -0,0 +1,32 @@
+package de.iani.cubesideutils.commands;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.util.StringUtil;
+
+public abstract class HybridCommand extends SubCommand implements CommandExecutor, TabCompleter {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
+        return onCommand(sender, command, alias, "/" + alias, new ArgsParser(args));
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        Collection<String> options = onTabComplete(sender, command, alias, new ArgsParser(args));
+        if (options == null) {
+            return null;
+        }
+
+        List<String> result = StringUtil.copyPartialMatches(args.length > 0 ? args[args.length - 1] : "", options, new ArrayList<String>());
+        Collections.sort(result);
+        return result;
+    }
+
+}
