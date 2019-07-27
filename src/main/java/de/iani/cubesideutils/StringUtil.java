613df701e82d346c66ebfce36bc595a5d674f54a
Jonas Becker <jonas.becker@uni-muenster.de>
2019-07-27 17:19:24 +0200

    Add MC_INDENT string.

--- src/main/java/de/iani/cubesideutils/StringUtil.java
+++ src/main/java/de/iani/cubesideutils/StringUtil.java
@@ -88,6 +88,8 @@ public class StringUtil {
         FALSE_STRINGS = Collections.unmodifiableSet(falseStrings);
     }
 
+    public static final String MC_INDENT = ChatColor.RESET + " ";
+
     public static final Pattern SPACES_AND_UNDERSCORES_PATTERN = Pattern.compile("[\\ \\_]");
 
     /**
