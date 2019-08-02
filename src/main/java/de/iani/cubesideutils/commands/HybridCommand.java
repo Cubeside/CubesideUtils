e118c7ceb674eeab2382112b3256ab12248e54b2
Jonas Becker <jonas.becker@uni-muenster.de>
2019-08-02 23:09:51 +0200

    Add checks for requiresPlayer and allowsCommandBlock.

--- src/main/java/de/iani/cubesideutils/commands/HybridCommand.java
+++ src/main/java/de/iani/cubesideutils/commands/HybridCommand.java
@@ -5,28 +5,45 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import org.bukkit.ChatColor;
+import org.bukkit.command.BlockCommandSender;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.minecart.CommandMinecart;
 import org.bukkit.util.StringUtil;
 
 public abstract class HybridCommand extends SubCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
-        if (this.getRequiredPermission() != null && !sender.hasPermission(getRequiredPermission())) {
+        if (!allowsCommandBlock() && (sender instanceof BlockCommandSender || sender instanceof CommandMinecart)) {
+            sender.sendMessage(ChatColor.RED + "This command is not allowed for CommandBlocks!");
+        }
+        if (requiresPlayer() && !(sender instanceof Player)) {
+            sender.sendMessage(ChatColor.RED + "This command can only be executed by players!");
+        }
+        if (getRequiredPermission() != null && !sender.hasPermission(getRequiredPermission())) {
             sender.sendMessage(ChatColor.RED + "No permission!");
             return true;
         }
+
         return onCommand(sender, command, alias, "/" + alias, new ArgsParser(args));
     }
 
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
-        if (this.getRequiredPermission() != null && !sender.hasPermission(getRequiredPermission())) {
+        if (!allowsCommandBlock() && (sender instanceof BlockCommandSender || sender instanceof CommandMinecart)) {
             return Collections.emptyList();
         }
+        if (requiresPlayer() && !(sender instanceof Player)) {
+            return Collections.emptyList();
+        }
+        if (getRequiredPermission() != null && !sender.hasPermission(getRequiredPermission())) {
+            return Collections.emptyList();
+        }
+
         Collection<String> options = onTabComplete(sender, command, alias, new ArgsParser(args));
         if (options == null) {
             return null;
