bf4cb4d7bfa504714f2791697d7f2a16036b8731
Jonas Becker <jonas.becker@uni-muenster.de>
2019-09-08 13:28:36 +0200

    Small changes to sendMessagesPaged. # Conflicts: #	src/main/java/de/iani/cubesideutils/ChatUtil.java

--- src/main/java/de/iani/cubesideutils/ChatUtil.java
+++ src/main/java/de/iani/cubesideutils/ChatUtil.java
@@ -117,11 +117,20 @@ public class ChatUtil {
     }
 
     public static void sendMessagesPaged(CommandSender recipient, List<? extends Sendable> messages, int page, String name, String openPageCommandPrefix, String pluginPrefix, ChatColor normalColor, ChatColor warningColor) {
+        if (page < 0) {
+            sendMessage(recipient, pluginPrefix, warningColor.toString(), "Bitte gib die Seitenzahl als positive ganze Zahl an.");
+            return;
+        }
+
         int listSize = messages.size();
         int numPages = (int) Math.ceil(listSize / (double) PAGE_LENGTH);
         if (page >= numPages && page > 0) {
             sendMessage(recipient, pluginPrefix, warningColor.toString(), name, " hat keine Seite ", (page + 1));
             return;
+        }
+
+        if (!openPageCommandPrefix.startsWith("/")) {
+            openPageCommandPrefix = "/" + openPageCommandPrefix;
         }
 
         if (numPages > 1) {
