6561f3289e59973c12304ce06fce97b22bbbc2e7
Jonas Becker <jonas.becker@uni-muenster.de>
2020-02-08 01:36:48 +0100

    Add roundTimespan and roundDate.

--- src/main/java/de/iani/cubesideutils/StringUtil.java
+++ src/main/java/de/iani/cubesideutils/StringUtil.java
@@ -5,6 +5,7 @@ import java.io.StringWriter;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -767,6 +768,39 @@ public class StringUtil {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         return timestampFormat.format(date);
+    }
+
+    public static long roundTimespan(long ms, ChronoUnit unit) {
+        long roundingDuration = unit.getDuration().toMillis();
+        long remainder = ms % roundingDuration;
+        long quotient = ms / roundingDuration;
+
+        if (remainder > 0) {
+            quotient++;
+        }
+
+        return quotient * roundingDuration;
+    }
+
+    public static long roundDate(long ms, ChronoUnit unit) {
+        long roundingDuration = unit.getDuration().toMillis();
+        long remainder = ms % roundingDuration;
+        long quotient = ms / roundingDuration;
+
+        // round down for days and longer, round down otherwise
+        if (unit.isDateBased() && remainder < 0) {
+            // days shall be rounded down, but negative values are roundet up (towards 0) by default
+            quotient--;
+        } else if (!unit.isDateBased() && remainder > 0) {
+            // shorter than days shall be rounded up, but positive values are roundet down (towards 0) by default
+            quotient++;
+        }
+
+        return quotient * roundingDuration;
+    }
+
+    public static Date roundDate(Date date, ChronoUnit unit) {
+        return new Date(roundDate(date.getTime(), unit));
     }
 
     // Colors
