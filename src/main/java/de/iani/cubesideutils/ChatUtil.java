07f0877da57b3a1ac699022fb47a74236d59e823
Jonas Becker <jonas.becker@uni-muenster.de>
2020-02-23 18:40:20 +0100

    Implement CachedSendableList to allow use of sendMessagesPaged with lazy queries. # Conflicts: #	src/main/java/de/iani/cubesideutils/ChatUtil.java

--- src/main/java/de/iani/cubesideutils/ChatUtil.java
+++ src/main/java/de/iani/cubesideutils/ChatUtil.java
@@ -1,8 +1,12 @@
 package de.iani.cubesideutils;
 
+import java.util.AbstractList;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.function.BiFunction;
+import java.util.function.IntSupplier;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
@@ -49,6 +53,43 @@ public class ChatUtil {
         }
     }
 
+    public static class CachedSendableList<T extends Sendable> extends AbstractList<T> {
+
+        private IntSupplier sizeGetter;
+        private BiFunction<Integer, Integer, List<T>> listFiller;
+
+        private int cacheSize;
+        private int cacheStartIndex;
+        private List<T> cache;
+
+        public CachedSendableList(IntSupplier sizeGetter, BiFunction<Integer, Integer, List<T>> listFiller, int cacheSize) {
+            this.sizeGetter = sizeGetter;
+            this.listFiller = listFiller;
+            this.cacheSize = cacheSize;
+            this.cache = Collections.emptyList();
+        }
+
+        public CachedSendableList(IntSupplier sizeGetter, BiFunction<Integer, Integer, List<T>> listFiller) {
+            this(sizeGetter, listFiller, PAGE_LENGTH);
+        }
+
+        @Override
+        public T get(int index) {
+            int transformedIndex = index - cacheStartIndex;
+            if (transformedIndex < 0 || transformedIndex >= cacheSize) {
+                cache = listFiller.apply(index, index + cacheSize);
+                cacheStartIndex = index;
+            }
+            return cache.get(transformedIndex);
+        }
+
+        @Override
+        public int size() {
+            return sizeGetter.getAsInt();
+        }
+
+    }
+
     public static List<Sendable> stringToSendableList(List<String> msges) {
         ArrayList<Sendable> result = new ArrayList<>(msges.size());
         for (String msg : msges) {
@@ -67,35 +108,34 @@ public class ChatUtil {
 
     public static final int PAGE_LENGTH = 10;
 
-    public static void sendMessagesPaged(CommandSender recipient, List<? extends Sendable> messages,
-            int page, String name, String openPageCommandPrefix) {
+    public static void sendMessagesPaged(CommandSender recipient, List<? extends Sendable> messages, int page, String name, String openPageCommandPrefix) {
         sendMessagesPaged(recipient, messages, page, name, openPageCommandPrefix, "");
     }
 
-    public static void sendMessagesPaged(CommandSender recipient, List<? extends Sendable> messages,
-            int page, String name, String openPageCommandPrefix, String pluginPrefix) {
-        sendMessagesPaged(recipient, messages, page, name, openPageCommandPrefix, pluginPrefix, ChatColor.GREEN,
-                ChatColor.GOLD);
+    public static void sendMessagesPaged(CommandSender recipient, List<? extends Sendable> messages, int page, String name, String openPageCommandPrefix, String pluginPrefix) {
+        sendMessagesPaged(recipient, messages, page, name, openPageCommandPrefix, pluginPrefix, ChatColor.GREEN, ChatColor.GOLD);
     }
 
-    public static void sendMessagesPaged(CommandSender recipient, List<? extends Sendable> messages,
-            int page, String name, String openPageCommandPrefix, String pluginPrefix, ChatColor normalColor,
-            ChatColor warningColor) {
-
-        int numPages = (int) Math.ceil(messages.size() / (double) PAGE_LENGTH);
-        if (page >= numPages) {
+    public static void sendMessagesPaged(CommandSender recipient, List<? extends Sendable> messages, int page, String name, String openPageCommandPrefix, String pluginPrefix, ChatColor normalColor, ChatColor warningColor) {
+        int listSize = messages.size();
+        int numPages = (int) Math.ceil(listSize / (double) PAGE_LENGTH);
+        if (page >= numPages && page > 0) {
             sendMessage(recipient, pluginPrefix, warningColor.toString(), name, " hat keine Seite ", (page + 1));
             return;
         }
 
         if (numPages > 1) {
-            sendMessage(recipient, pluginPrefix, normalColor.toString(), name, " (Seite ", (page + 1), "/",  numPages, "):");
+            sendMessage(recipient, pluginPrefix, normalColor.toString(), name, " (Seite ", (page + 1), "/", numPages, "):");
         } else {
             sendMessage(recipient, pluginPrefix, normalColor.toString(), name, ":");
         }
 
+        if (listSize == 0) {
+            recipient.sendMessage(ChatColor.GRAY + " -- keine --");
+        }
+
         int index = page * PAGE_LENGTH;
-        for (int i = 0; i < PAGE_LENGTH && index < messages.size();) {
+        for (int i = 0; i < PAGE_LENGTH && index < listSize;) {
             messages.get(index).send(recipient);
 
             i++;
@@ -108,17 +148,14 @@ public class ChatUtil {
             if (page > 0) {
                 builder.color(ChatColor.BLUE);
 
-                HoverEvent he = new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                        new ComponentBuilder("Seite " + page + " anzeigen").create());
-                ClickEvent ce = new ClickEvent(ClickEvent.Action.RUN_COMMAND,
-                        openPageCommandPrefix + " " + page);
+                HoverEvent he = new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Seite " + page + " anzeigen").create());
+                ClickEvent ce = new ClickEvent(ClickEvent.Action.RUN_COMMAND, openPageCommandPrefix + " " + page);
 
                 builder.event(he).event(ce);
             } else {
                 builder.color(ChatColor.GRAY);
 
-                HoverEvent he = new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                        new ComponentBuilder("Bereits auf Seite 1").create());
+                HoverEvent he = new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Bereits auf Seite 1").create());
 
                 builder.event(he);
             }
@@ -128,17 +165,14 @@ public class ChatUtil {
             if (page + 1 < numPages) {
                 builder.color(ChatColor.BLUE);
 
-                HoverEvent he = new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                        new ComponentBuilder("Seite " + (page + 2) + " anzeigen").create());
-                ClickEvent ce = new ClickEvent(ClickEvent.Action.RUN_COMMAND,
-                        openPageCommandPrefix + " " + (page + 2));
+                HoverEvent he = new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Seite " + (page + 2) + " anzeigen").create());
+                ClickEvent ce = new ClickEvent(ClickEvent.Action.RUN_COMMAND, openPageCommandPrefix + " " + (page + 2));
 
                 builder.event(he).event(ce);
             } else {
                 builder.color(ChatColor.GRAY);
 
-                HoverEvent he = new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                        new ComponentBuilder("Bereits auf Seite " + numPages).create());
+                HoverEvent he = new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder("Bereits auf Seite " + numPages).create());
 
                 builder.event(he);
             }
@@ -147,12 +181,11 @@ public class ChatUtil {
         }
     }
 
-    public static void sendMessage(CommandSender sender, String pluginPrefix, String colors, Object message,
-            Object... messageParts) {
+    public static void sendMessage(CommandSender sender, String pluginPrefix, String colors, Object message, Object... messageParts) {
         if (messageParts.length == 0) {
-            sender.sendMessage(pluginPrefix + " " + (colors == null? "" : colors) + message);
+            sender.sendMessage(pluginPrefix + " " + (colors == null ? "" : colors) + message);
         } else {
-            StringBuilder builder = new StringBuilder(pluginPrefix).append(" ").append(colors == null? "" : colors).append(message);
+            StringBuilder builder = new StringBuilder(pluginPrefix).append(" ").append(colors == null ? "" : colors).append(message);
             for (Object s : messageParts) {
                 if (colors != null) {
                     builder.append(ChatColor.RESET).append(colors);
