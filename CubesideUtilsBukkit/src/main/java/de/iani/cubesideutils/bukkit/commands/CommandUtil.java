f5afad78fcff0ca745b28a9154cd4fa078c0de8c
Brokkonaut <hannos17@gmx.de>
2020-10-11 16:34:18 +0200

    Add methods for registering commands (moved from CubesideEssentials)

--- CubesideUtilsBukkit/src/main/java/de/iani/cubesideutils/bukkit/commands/CommandUtil.java
+++ CubesideUtilsBukkit/src/main/java/de/iani/cubesideutils/bukkit/commands/CommandUtil.java
@@ -1,15 +1,28 @@
 package de.iani.cubesideutils.bukkit.commands;
 
+import com.google.common.base.Preconditions;
+import de.iani.cubesideutils.collections.IteratorUtil;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Level;
 import org.bukkit.Bukkit;
 import org.bukkit.Server;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.SimpleCommandMap;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.plugin.Plugin;
 
 public class CommandUtil {
     private CommandUtil() {
         throw new UnsupportedOperationException("No instance for you, Sir!");
         // prevents instances
     }
+
+    public static final TabCompleter EMPTY_TAB_COMPLETER = (sender, command, alias, args) -> Collections.emptyList();
 
     /**
      * This method has to be called after commands are added / removed in the servers command map.
@@ -24,5 +37,50 @@ public class CommandUtil {
         } catch (Exception e) {
             server.getLogger().log(Level.SEVERE, "Could not resync commands", e);
         }
+    }
+
+    public static boolean registerCommand(Plugin plugin, String command, CommandExecutor executor, TabCompleter completer) {
+        return registerCommand(plugin, command, executor, completer, false);
+    }
+
+    public static boolean registerCommand(Plugin plugin, String command, CommandExecutor executor, TabCompleter completer, boolean replaceExisting) {
+        return registerCommand(plugin, command, Collections.emptyList(), executor, completer, replaceExisting);
+    }
+
+    public static boolean registerCommand(Plugin plugin, String command, List<String> aliases, CommandExecutor executor, TabCompleter completer) {
+        return registerCommand(plugin, command, aliases, executor, completer, false);
+    }
+
+    public static boolean registerCommand(Plugin plugin, String command, List<String> aliases, CommandExecutor executor, TabCompleter completer, boolean replaceExisting) {
+        Preconditions.checkNotNull(plugin, "plugin");
+        Preconditions.checkNotNull(command, "command");
+        Preconditions.checkNotNull(aliases, "aliases");
+        Preconditions.checkNotNull(executor, "executor");
+
+        SimpleCommandMap commandMap = (SimpleCommandMap) Bukkit.getCommandMap();
+        if (replaceExisting) {
+            for (String alias : IteratorUtil.concat(Collections.singleton(command), aliases)) {
+                if (commandMap.getKnownCommands().remove(alias) != null) {
+                    plugin.getLogger().log(Level.INFO, "Replacing command /" + alias + ".");
+                }
+            }
+        }
+
+        Command cmd = new Command(command) {
+            @Override
+            public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+                return executor.onCommand(sender, this, commandLabel, args);
+            }
+
+            @Override
+            public List<String> tabComplete(CommandSender sender, String alias, String[] args) throws IllegalArgumentException {
+                return completer == null ? super.tabComplete(sender, alias, args) : completer.onTabComplete(sender, this, alias, args);
+            }
+        }.setAliases(new ArrayList<>(aliases));
+
+        boolean result = commandMap.register(plugin.getDescription().getName(), cmd);
+        resyncCommandTabCompletions();
+
+        return result;
     }
 }
