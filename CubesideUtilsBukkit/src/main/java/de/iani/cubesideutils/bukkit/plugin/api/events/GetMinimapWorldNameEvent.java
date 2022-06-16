36904c746aa213e045d163af568ac48e603dfba6
Brokkonaut <hannos17@gmx.de>
2022-06-16 05:45:17 +0200

    Add GetMinimapWorldNameEvent for use by CubesideEssentials

---
+++ CubesideUtilsBukkit/src/main/java/de/iani/cubesideutils/bukkit/plugin/api/events/GetMinimapWorldNameEvent.java
@@ -0,0 +1,43 @@
+package de.iani.cubesideutils.bukkit.plugin.api.events;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.player.PlayerEvent;
+
+public class GetMinimapWorldNameEvent extends PlayerEvent {
+    private static final HandlerList handlers = new HandlerList();
+
+    private String serverName;
+    private String worldName;
+
+    public GetMinimapWorldNameEvent(Player player, String serverName, String worldName) {
+        super(player);
+        this.serverName = serverName;
+        this.worldName = worldName;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public void setServerName(String serverName) {
+        this.serverName = serverName;
+    }
+
+    public String getWorldName() {
+        return worldName;
+    }
+
+    public void setWorldName(String worldName) {
+        this.worldName = worldName;
+    }
+}
