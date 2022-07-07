7bfea4e8493bdfde941a30b458b7eb452200e0a1
Brokkonaut <hannos17@gmx.de>
2022-07-07 03:10:47 +0200

    add some new custom heads and improve api

--- CubesideUtilsBukkit/src/main/java/de/iani/cubesideutils/bukkit/items/CustomHeads.java
+++ CubesideUtilsBukkit/src/main/java/de/iani/cubesideutils/bukkit/items/CustomHeads.java
@@ -2,6 +2,7 @@ package de.iani.cubesideutils.bukkit.items;
 
 import com.destroystokyo.paper.profile.PlayerProfile;
 import com.destroystokyo.paper.profile.ProfileProperty;
+import java.util.Arrays;
 import java.util.UUID;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -18,7 +19,10 @@ public enum CustomHeads {
     QUARTZ_PLUS("bafea105-1289-46d4-a3a6-822bc1cd4bd7", "Quartz Plus", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNDdhMGZjNmRjZjczOWMxMWZlY2U0M2NkZDE4NGRlYTc5MWNmNzU3YmY3YmQ5MTUzNmZkYmM5NmZhNDdhY2ZiIn19fQ=="),
     QUARTZ_MINUS("37fa8deb-3207-4f29-9823-10316eb12ec7", "Quartz Minus", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvOWQ2YjEyOTNkYjcyOWQwMTBmNTM0Y2UxMzYxYmJjNTVhZTVhOGM4ZjgzYTE5NDdhZmU3YTg2NzMyZWZjMiJ9fX0="),
     QUARTZ_CHECKMARK("ac8bc90d-88dc-4ef3-8e4f-14c60cf41709", "Quartz Checkmark", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzdiNjJkMjc1ZDg3YzA5Y2UxMGFjYmNjZjM0YzRiYTBiNWYxMzVkNjQzZGM1MzdkYTFmMWRmMzU1YTIyNWU4MiJ9fX0="),
+    QUARTZ_QUESTION_MARK("db30a037-bf94-421a-a2bd-8fe7f5d76dd9", "Quartz Question Mark", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvZmMyNzEwNTI3MTllZjY0MDc5ZWU4YzE0OTg5NTEyMzhhNzRkYWM0YzI3Yjk1NjQwZGI2ZmJkZGMyZDZiNWI2ZSJ9fX0="),
+    QUARTZ_EXCLAMATION_MARK("f56bc536-4a0a-4c8f-900c-226bf1d9a001", "Quartz Exclamation Mark", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvN2I4N2QyMjUyY2FjMWFhMTVkZjMyNTk5OGI4ZWM4MmVmOTEwOWI2YzU2NzYxMGFmYWMwZWNkYTUxM2Y2MSJ9fX0="),
     QUARTZ_X("51bdc491-3e72-4c1b-af6d-a0db5e4a2bc7", "Quartz X", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMzkxZDZlZGE4M2VkMmMyNGRjZGNjYjFlMzNkZjM2OTRlZWUzOTdhNTcwMTIyNTViZmM1NmEzYzI0NGJjYzQ3NCJ9fX0="),
+    TNT("f0284b28-c50b-4494-b0d7-124c6e8a24ce", "TNT", "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvNTBlNGM2NzBjOWIyYzE5YTQ4YTkyMzExYThkN2Y4MmEzOTY1YmMyOTdhMjIwYzg5ZTE2NjgyNTQxN2U4In19fQ=="),
     ;
 
     private ItemStack head;
@@ -32,9 +36,16 @@ public enum CustomHeads {
     }
 
     public ItemStack getHead(String displayName) {
+        return getHead(displayName, null);
+    }
+
+    public ItemStack getHead(String displayName, String... lore) {
         ItemStack stack = getHead();
         ItemMeta meta = stack.getItemMeta();
         meta.setDisplayName(displayName);
+        if (lore != null && lore.length > 0) {
+            meta.setLore(Arrays.asList(lore));
+        }
         stack.setItemMeta(meta);
         return stack;
     }
