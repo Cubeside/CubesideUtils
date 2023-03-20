6077bd3f5440bf27950fb3d6fdc207798f5a94fa
Brokkonaut <hannos17@gmx.de>
2023-03-20 04:11:00 +0100

    Add method filterIncompleteSurrogatePairs

--- CubesideUtilsCore/src/main/java/de/iani/cubesideutils/StringUtilCore.java
+++ CubesideUtilsCore/src/main/java/de/iani/cubesideutils/StringUtilCore.java
@@ -18,4 +18,50 @@ public class StringUtilCore {
     };
 
     public static final BiPredicate<String, String> CASE_IGNORING_EQUALITY = (s1, s2) -> s1 == null ? s2 == null : s1.equalsIgnoreCase(s2);
+
+    /**
+     * Filters all incomplete surrogates from a string while keeping all other chars.
+     * If no incomplete surrogates are found the input string is returned.
+     *
+     * @param input
+     *            a string
+     * @return a variant of the input string where all incomplete surrogates are removed.
+     */
+    public static String filterIncompleteSurrogatePairs(String input) {
+        StringBuilder sb = null;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if (c < Character.MIN_SURROGATE || c > Character.MAX_SURROGATE) {
+                // no surrogate, so always valid
+                if (sb != null) {
+                    sb.append(c);
+                }
+            } else {
+                if (c >= Character.MIN_HIGH_SURROGATE && c <= Character.MAX_HIGH_SURROGATE && i + 1 < input.length()) {
+                    char next = input.charAt(i + 1);
+                    if (next >= Character.MIN_LOW_SURROGATE && next <= Character.MAX_LOW_SURROGATE) {
+                        // valid surrogate pair
+                        i++;
+                        if (sb != null) {
+                            sb.append(c);
+                            sb.append(next);
+                        }
+                    } else {
+                        // missing low surrogate, skip the high surrogate
+                        if (sb == null) {
+                            sb = new StringBuilder();
+                            sb.append(input, 0, i);
+                        }
+                    }
+                } else {
+                    // low surrogate without a previous high surrogate or high surrogate at the end of the string, skip
+                    if (sb == null) {
+                        sb = new StringBuilder();
+                        sb.append(input, 0, i);
+                    }
+                }
+            }
+        }
+        return sb != null ? sb.toString() : input;
+    }
 }
