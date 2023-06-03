0849f5dbe1c9f46f15ec2e3bf707bea87f317a34
Brokkonaut <hannos17@gmx.de>
2023-06-03 05:48:16 +0200

    improve surrogate filtering and add filterControlCharacters

--- CubesideUtilsCore/src/main/java/de/iani/cubesideutils/StringUtilCore.java
+++ CubesideUtilsCore/src/main/java/de/iani/cubesideutils/StringUtilCore.java
@@ -40,11 +40,19 @@ public class StringUtilCore {
                 if (c >= Character.MIN_HIGH_SURROGATE && c <= Character.MAX_HIGH_SURROGATE && i + 1 < input.length()) {
                     char next = input.charAt(i + 1);
                     if (next >= Character.MIN_LOW_SURROGATE && next <= Character.MAX_LOW_SURROGATE) {
-                        // valid surrogate pair
                         i++;
-                        if (sb != null) {
-                            sb.append(c);
-                            sb.append(next);
+                        if (Character.isDefined(Character.toCodePoint(c, next))) {
+                            // valid surrogate pair
+                            if (sb != null) {
+                                sb.append(c);
+                                sb.append(next);
+                            }
+                        } else {
+                            // undefined character, skip both
+                            if (sb == null) {
+                                sb = new StringBuilder();
+                                sb.append(input, 0, i - 1);
+                            }
                         }
                     } else {
                         // missing low surrogate, skip the high surrogate
@@ -59,6 +67,33 @@ public class StringUtilCore {
                         sb = new StringBuilder();
                         sb.append(input, 0, i);
                     }
+                }
+            }
+        }
+        return sb != null ? sb.toString() : input;
+    }
+
+    /**
+     * Filters all control characters from a string
+     *
+     * @param input
+     *            a string
+     * @param allowNewline
+     *            allow new lines in the string or filter them
+     * @return a variant of the input string where all control characters are removed.
+     */
+    public static String filterControlCharacters(String input, boolean allowNewline) {
+        StringBuilder sb = null;
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if ((c < 32 && (!allowNewline || c != '\n')) || (c >= 127 && c < 160)) {
+                if (sb == null) {
+                    sb = new StringBuilder();
+                    sb.append(input, 0, i);
+                }
+            } else {
+                if (sb != null) {
+                    sb.append(c);
                 }
             }
         }
