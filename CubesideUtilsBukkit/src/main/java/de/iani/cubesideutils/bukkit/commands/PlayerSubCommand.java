1a54f47c6625754f31b1409a5e74ea1384d1c227
Brokkonaut <hannos17@gmx.de>
2024-12-11 06:57:00 +0100

    add onTabComplete to PlayerSubCommand

--- CubesideUtilsBukkit/src/main/java/de/iani/cubesideutils/bukkit/commands/PlayerSubCommand.java
+++ CubesideUtilsBukkit/src/main/java/de/iani/cubesideutils/bukkit/commands/PlayerSubCommand.java
@@ -6,6 +6,8 @@ import de.iani.cubesideutils.bukkit.commands.exceptions.InternalCommandException
 import de.iani.cubesideutils.bukkit.commands.exceptions.NoPermissionException;
 import de.iani.cubesideutils.bukkit.commands.exceptions.RequiresPlayerException;
 import de.iani.cubesideutils.commands.ArgsParser;
+import java.util.Collection;
+import java.util.List;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -25,4 +27,16 @@ public abstract class PlayerSubCommand extends SubCommand {
     }
 
     public abstract boolean onCommand(Player sender, Command command, String alias, String commandString, ArgsParser args) throws DisallowsCommandBlockException, RequiresPlayerException, NoPermissionException, IllegalSyntaxException, InternalCommandException;
+
+    @Override
+    public final Collection<String> onTabComplete(CommandSender sender, Command command, String alias, ArgsParser args) {
+        if (sender instanceof Player player) {
+            return onTabComplete(player, command, alias, args);
+        }
+        return List.of();
+    }
+
+    public Collection<String> onTabComplete(Player sender, Command command, String alias, ArgsParser args) {
+        return super.onTabComplete(sender, command, alias, args);
+    }
 }
