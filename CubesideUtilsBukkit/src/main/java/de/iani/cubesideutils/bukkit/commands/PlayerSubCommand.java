dedb740dc94b851d8e018a1f09abc608dab14270
Brokkonaut <hannos17@gmx.de>
2026-04-16 22:02:50 +0200

    add some more player methods

--- CubesideUtilsBukkit/src/main/java/de/iani/cubesideutils/bukkit/commands/PlayerSubCommand.java
+++ CubesideUtilsBukkit/src/main/java/de/iani/cubesideutils/bukkit/commands/PlayerSubCommand.java
@@ -19,6 +19,42 @@ public abstract class PlayerSubCommand extends SubCommand {
     }
 
     @Override
+    public final boolean hasRequiredPermission(CommandSender sender) {
+        return sender instanceof Player player ? hasRequiredPermission(player) : false;
+    }
+
+    public boolean hasRequiredPermission(Player sender) {
+        return getRequiredPermission() == null || sender.hasPermission(getRequiredPermission());
+    }
+
+    @Override
+    public final boolean isAvailable(CommandSender sender) {
+        return sender instanceof Player player ? isAvailable(player) : false;
+    }
+
+    public boolean isAvailable(Player sender) {
+        return true;
+    }
+
+    @Override
+    public final boolean isVisible(CommandSender sender) {
+        return sender instanceof Player player ? isVisible(player) : false;
+    }
+
+    public boolean isVisible(Player sender) {
+        return true;
+    }
+
+    @Override
+    public final String getUsage(CommandSender sender) {
+        return sender instanceof Player player ? getUsage(player) : "";
+    }
+
+    public String getUsage(Player sender) {
+        return getUsage();
+    }
+
+    @Override
     public final boolean onCommand(CommandSender sender, Command command, String alias, String commandString, ArgsParser args) throws DisallowsCommandBlockException, RequiresPlayerException, NoPermissionException, IllegalSyntaxException, InternalCommandException {
         if (sender instanceof Player player) {
             return onCommand(player, command, alias, commandString, args);
